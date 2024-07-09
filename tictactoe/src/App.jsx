5707b000add922c0f0d3b2f0eadc7eebb714e6a0
Nikodem <nikodem.faber@interia.pl>
2024-07-09 10:49:55 +0200

    Created component Player and added it into App component

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,10 +1,18 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
-  return (
-   <p>Comming soon</p>
-  )
+	return (
+		<main>
+			<div id="game-container">
+				<ol id="players">
+					<Player name="Player 1" symbol="X" />
+					<Player name="Player 2" symbol="0" />
+				</ol>
+				GAME BOARD
+			</div>
+			LOG
+		</main>
+	);
 }
 
-export default App
+export default App;
