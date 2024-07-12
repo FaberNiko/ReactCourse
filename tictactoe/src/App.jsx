72a9b7a3a9cf6e3ba96fa35f8d49f8102d87d11b
Nikodem <nikodem.faber@interia.pl>
2024-07-12 08:56:36 +0200

    Made Log component, lifted states

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,24 +1,49 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import { useState } from "react";
-
+import Log from "./components/Log";
 function App() {
+	const [activePlayer, setActivePlayer] = useState("X");
+	const [gameTurns, setGameTurns] = useState([]);
 
-	const [activePlayer, setActivePlayer] = useState('X')
+	function handleSelectSquare(rowIndex, colIndex) {
+		setActivePlayer(curActivePlayer => (curActivePlayer === "X" ? "O" : "X")); // Checking which player has move
+		setGameTurns((prevTurns) => { // Copying existing turns to new const
+			let currentPlayer = "X";
 
-	function handleSelectSquare() {
-		setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X' )
+			if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+				currentPlayer = 'O';
+			}
+
+			const updatedTurns = [
+				{ square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+				...prevTurns,
+			]; 
+
+			return updatedTurns;
+		});
 	}
 	return (
 		<main>
 			<div id="game-container">
 				<ol id="players" className="highlight-player">
-					<Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-					<Player initialName="Player 2" symbol="0" isActive={activePlayer === 'O'}/>
+					<Player
+						initialName="Player 1"
+						symbol="X"
+						isActive={activePlayer === "X"}
+					/>
+					<Player
+						initialName="Player 2"
+						symbol="0"
+						isActive={activePlayer === "O"}
+					/>
 				</ol>
-				<GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer}/>
+				<GameBoard
+					onSelectSquare={handleSelectSquare}
+					turns = {gameTurns}
+				/>
 			</div>
-			LOG
+			<Log turns={gameTurns}/>
 		</main>
 	);
 }
