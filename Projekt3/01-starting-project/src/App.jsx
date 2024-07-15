6eb9653ba3c8bfd37fe0ebbf4b116a0de7c40209
Nikodem <nikodem.faber@interia.pl>
2024-07-15 11:00:47 +0200

    Updated app component

--- Projekt3/01-starting-project/src/App.jsx
+++ Projekt3/01-starting-project/src/App.jsx
@@ -11,6 +11,8 @@ function App() {
 		duration: 10,
 	});
 
+    const inputIsValid = userInput.duration >= 1
+
 	function handleChange(inputIdentifier, newValue) {
 		setUserInput(prevUserInput => {
 			return {
@@ -19,11 +21,14 @@ function App() {
 			};
 		});
 	}
+
+
 	return (
 		<>
 			<Header />
 			<UserInput onChange={handleChange} userInput={userInput} />
-            <Results input={userInput}/>
+            {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+            {inputIsValid && <Results input={userInput}/>}
 		</>
 	);
 }
